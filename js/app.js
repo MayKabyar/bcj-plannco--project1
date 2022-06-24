087cde0c87e63fb30ee89978ea76849c87b7bc6a
May Kabyar <kabitarmg21@gmail.com>
2022-06-24 20:33:38 +0630

    nxt

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 
+	// Start Header
+
+	// Start nav
+
+	$('.navbuttons').click(function(){
+		$('.navbuttons').toggleClass('crossxs');
+	});
+
+	// End nav
+
+	// Start Header
 });
